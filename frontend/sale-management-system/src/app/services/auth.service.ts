17674381d011d9cb82aa98f9d0e88d27b82dd8a0
Aicha Louraghi <a.louraghi@esi-sba.dz>
2021-10-11 19:44:11 +0200

    updating the services with functions,adding the helper class and adding bootstrap as link in index.html

--- frontend/sale-management-system/src/app/services/auth.service.ts
+++ frontend/sale-management-system/src/app/services/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+const AUTH_API = 'http://localhost:8081/api/auth/';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  login(credentials): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signin',
+      {
+        username: credentials.username,
+        password: credentials.password,
+      },
+      httpOptions
+    );
+  }
+
+  register(user): Observable<any> {
+    return this.http.post(
+      AUTH_API + 'signup',
+      {
+        username: user.username,
+        email: user.email,
+        password: user.password,
+      },
+      httpOptions
+    );
+  }
 }
--- frontend/sale-management-system/src/app/services/user.service.ts
+++ frontend/sale-management-system/src/app/services/user.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+const API_URL = 'http://localhost:8081/api/test/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPublicContent(): Observable<any> {
+    return this.http.get(API_URL + 'all', { responseType: 'text' });
+  }
+
+  getFournisseurBoard(): Observable<any> {
+    return this.http.get(API_URL + 'fournisseur', { responseType: 'text' });
+  }
+
+  getAdminBoard(): Observable<any> {
+    return this.http.get(API_URL + 'responsable', { responseType: 'text' });
+  }
 }
